e783d92bf76aa1f6ec57be2c78fc6594250e4084
Maurício <mrc_augusto@hotmail.com>
2024-10-16 14:52:37 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'react-native';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar/>
+      <Home/>
+    </>
+  );
+}
+
+
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import {styles} from './styles';
+
+export function Home(){
+  return(
+    <View style={styles.container}>
+      <Text>Oi</Text>
+    </View>
+  )
+}
